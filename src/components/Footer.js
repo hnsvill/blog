d6f579a76694cb04dddea17bd5d3ee399bfae114
Hannah Scovill <hannah.scovill@kroger.com>
2019-01-04 16:10:03 -0800

    looking for ways to customize

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright">Photo Credit: <a href="https://unsplash.com/@srshah19?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank">Snehal Shah</a></p>
     </footer>
 )
 
