f68968ae8bd38c394fccc8361a5e8e83b6333816
Hannah Scovill <hannah.scovill@kroger.com>
2019-02-22 15:29:57 -0800

    removed action=#

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,24 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Contact = (props) => (
-    <form name="Contact" method="post" action="#" data-netlify="true" data-netlify-honeypot="bot-field">
-    <input type="hidden" name="bot-field" />
-    <div className="field half first">
-    <label htmlFor="name">Name</label>
-    <input type="text" name="name" id="name" />
-    </div>
-    <div className="field half">
-    <label htmlFor="email">Email</label>
-    <input type="text" name="email" id="email" />
-    </div>
-    <div className="field">
-    <label htmlFor="message">Message</label>
-    <textarea name="message" id="message" rows="4"></textarea>
-    </div>
-    <ul className="actions">
-    <li><input type="submit" value="Send Message" className="special" /></li>
-    <li><input type="reset" value="Reset" /></li>
-    </ul>
+    <form name="Contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="bot-field" />
+        <div className="field half first">
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" id="name" />
+        </div>
+        <div className="field half">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" id="email" />
+        </div>
+        <div className="field">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id="message" rows="4"></textarea>
+        </div>
+        <ul className="actions">
+            <li><input type="submit" value="Send Message" className="special" /></li>
+            <li><input type="reset" value="Reset" /></li>
+        </ul>
     </form>
 )
 
